71721dcff215a56856521c9a288765c9724e0dd8
Aurelien <aurelienmuset@gmail.com>
2022-01-17 22:15:54 +0100

    init

---
+++ js/app.js
@@ -0,0 +1,45 @@
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const min = document.getElementById('min');
+const sec = document.getElementById('sec');
+const daystxt = document.getElementById('daystxt');
+const hourstxt = document.getElementById('hourstxt');
+const mintxt = document.getElementById('mintxt');
+const sectxt = document.getElementById('sectxt');
+
+const currentDate = new Date().getFullYear();
+const dropFinish = new Date('January 16 2022 18:32:00');
+const txtSolde = document.getElementById('solde')
+
+
+
+function updCountodwn() {
+    const currentTime = new Date();
+    const diff = dropFinish - currentTime
+
+    const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+    const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+    const m = Math.floor(diff / 1000 / 60) % 60;
+    const s = Math.floor(diff / 1000) % 60;
+
+    days.innerHTML = d;
+    hours.innerHTML = h < 10 ? '0' + h : h;
+    min.innerHTML = m < 10 ? '0' + m : m;
+    sec.innerHTML = s < 10 ? '0' + s : s;
+
+    if (d<0 && h<0 && m<0 && s<0){
+        days.style.display="none"
+        hours.style.display="none"
+        min.style.display="none"
+        sec.style.display="none"
+        daystxt.style.display="none"
+        hourstxt.style.display="none"
+        mintxt.style.display="none"
+        sectxt.style.display="none"
+        txtSolde.style.display="none"
+    }
+
+
+}
+
+setInterval(updCountodwn, 1000);
---
+++ js/script.js
@@ -0,0 +1,48 @@
+let qte = document.getElementById('quantite')
+let plus = document.getElementById('plus')
+let moins = document.getElementById('moins')
+let n = 0
+qte.innerHTML = n
+
+
+plus.addEventListener('click', () => {
+    n += 1
+    qte.innerHTML = n
+})
+
+moins.addEventListener('click', () => {
+    if (n === 0) {
+        alert('Qte negative')
+    } else {
+        n = n - 1
+        qte.innerHTML = n
+    }
+})
+
+
+
+let prixreduc = document.getElementById('prixReduc')
+let ajtPanier = document.getElementById('ajouterPanier')
+let qtePanier = document.getElementById('qte-panier')
+let vPrix = 125;
+prixreduc.innerHTML = vPrix
+
+ajtPanier.addEventListener('click', () => {
+    
+    if (n === 0) {
+        alert("Vous devez choisir au moins 1 article")
+    } else {
+        qtePanier.innerHTML = n
+        nbChoix++;
+    } 
+
+
+
+
+
+
+        
+
+    
+  
+})
